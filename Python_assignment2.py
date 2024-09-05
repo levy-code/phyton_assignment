2e32158fdaca6aacc91a34ad18647d7291749851
Leslevy Arienza <arienzaleslevy@gmail.com>
2024-09-05 23:09:23 +1000

    python_assignment2

---
+++ Python_assignment2.py
@@ -0,0 +1,5 @@
+original_text = " I love Python "
+shouting_text = original_text.upper()
+annoying_text = shouting_text*10
+print(f"My annoying text is{annoying_text}")
+print(f"It has {len(annoying_text)} characters.")
