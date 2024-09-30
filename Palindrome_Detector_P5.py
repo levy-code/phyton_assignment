d21e7833e75ff2d6395a6d52eb242a8aa8896e2d
Leslevy Arienza <arienzaleslevy@gmail.com>
2024-09-30 22:37:29 +1000

    python assignment

--- Palindrome_Detector_P5.py
+++ Palindrome_Detector_P5.py
@@ -2,7 +2,7 @@ input_text = input("Please enter a word or sentence: ")
 cleaned_text = ""
 for char in input_text:
         if char.isalpha():
-                cleaned_text + char.lower()
+                cleaned_text += char.lower()
 input_words = list(cleaned_text)
 input_words.reverse()
 words_joined = "".join(input_words)
